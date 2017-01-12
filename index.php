98801f29597d6623ae29521537ee6107dd5f2574
Mason Crane <masonecrane@gmail.com>
2017-01-11 21:51:23 -0700

    added main tag and started creating content, and surrounding words with span tags to later apply CSS to make the content stand out.

--- index.php
+++ index.php
@@ -12,7 +12,14 @@
 			<h1>Persona, Use Case, Interaction Flow, and Conceptual Model</h1>
 		</header>
 		<main>
-
+			<h2>Persona</h2>
+			<p><span>Name:</span> Rafael</p>
+			<p><span>Age:</span> 24</p>
+			<p><span>Gender:</span> Male</p>
+			<p><span>Occupation:</span> Entrepreneur</p>
+			<p><span>Technology:</span> iPhone & Macbook Pro</p>
+			<p><span>Needs:</span> Web developer and entrepreneur who is using ProductHunt as a way to get the word out about his fledgling web app, in hopes that his web app will get enough attention to be highlighted on the front page and in ProductHunt's daily e-mail to thousands upon thousands of e-mail subscribers. Without a marketing budget Rafael knows that social proof on ProductHunt is a major milestone that needs to be accomplished for his new app to grow rapidly without spending money on advertising.</p>
+			<p><span>Goals:</span> Rafael has been actively participating on ProductHunt the entire time he has been creating his app MVP. He knows that having an established reputation and some solid connections on PH will help his app get in front of more eyes than it would without actively participating, because he knows that PH is a tight knit community of like-minded makers, entrepreneurs and developers.</p>
 		</main>
 	</body>
 </html>
