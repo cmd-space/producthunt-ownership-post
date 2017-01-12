e09a1cc20820d03faa47d9a84c603b8926839e15
Mason Crane <masonecrane@gmail.com>
2017-01-11 21:31:28 -0700

    Created index.php with skeleton template, header and h1

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport"
+				content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<title>ProductHunt Ownership Post Data Design</title>
+	</head>
+	<body>
+		<header>
+			<h1>Persona, Use Case, Interaction Flow, and Conceptual Model</h1>
+		</header>
+		<main>
+
+		</main>
+	</body>
+</html>
